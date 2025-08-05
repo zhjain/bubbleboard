befe3ecbc1775bca4132b8cf6088c412b1b31337
milky <1099738693@qq.com>
2025-08-05 17:18:28 +0800

    更新格式化配置，调整开发环境的HTTP端口和IP绑定

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,6 @@
 [
+  trailing_comma: :as_needed,
+  consolidate_aliases: true,
   import_deps: [:phoenix],
   plugins: [Phoenix.LiveView.HTMLFormatter],
   inputs: ["*.{heex,ex,exs}", "{config,lib,test}/**/*.{heex,ex,exs}"]
